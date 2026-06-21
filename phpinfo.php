162fbf1f923f2c95ac01265e671a5ddf2fdea569
cheng <cheng@wdubuntu.lan>
2026-06-21 08:18:36 +0800

    添加文件用于测试 phpinfo.php

---
+++ phpinfo.php
@@ -0,0 +1,11 @@
+<?php
+	/* $Id: Z_UploadUpdate.php ChengJiang $*/
+	/*
+ * @Author: ChengJiang 
+ * @Date: 2017-09-04 21:05:55 
+ * @Last Modified by: ChengJiang
+ * @Last Modified time: 2018-07-03 17:29:04
+    Release version2017-09-24
+ */
+ 	echo  phpinfo();	
+	?>
